4624c4b90a3d18c150d7f4289b9ce15a3e3e2b09
Danny Kaplan <kaplan@macalester.edu>
2018-08-28 10:36:04 -0500

    Added manual captioning for sds_table

--- R/SDS_table.R
+++ R/SDS_table.R
@@ -6,6 +6,7 @@
 #' @param data the data frame to be displayed
 #' @param show_n how many lines to display (default 6. Use `Inf` for all rows.)
 #' @param nrows an integer specifying how many rows the from the data frame should be displayed in the footnote. Set to `Inf` to suppress footnote.
+#' @param footnote character string to be placed at the bottom of the table in place of the usual "... and so on for __ rows."
 #' @param format either `"html"` or `"latex"`. Default will use `knitr::is_latex_output()` to decide.
 #' @param caption string to pass as a caption to the table
 #' @param ... additional arguments to `kableExtra::kable_styling()`, e.g. `"striped"`, `"hover"`
@@ -13,17 +14,17 @@
 #' sds_table(mtcars, "striped", "hover")
 #' sds_table(mtcars, show_n = 3, caption="Data about cars")
 #' @export
-sds_table <- function(data, show_n = 6L, nrows = nrow(data),
+sds_table <- function(data, show_n = 6L, nrows = nrow(data), footnote = NULL,
                       format=ifelse(knitr::is_latex_output(), "latex", "html"), caption="", ...) {
   res <-
     knitr::kable(head(data, min(c(nrows, show_n))),
                  format = format, caption=caption, row.names = FALSE) %>%
     kableExtra::kable_styling(full_width = FALSE, ...)
-  if (nrows > show_n) {
-    res <- res %>%
-      kableExtra::footnote(general_title = sprintf("... and so on for %s rows altogether.",
-                                                   prettyNum(nrows, big.mark = ",")),
-                                                   general = "")
+
+  if (is.null(footnote) && nrows > show_n) {
+      footnote <- sprintf("... and so on for %s rows altogether.", prettyNum(nrows, big.mark = ","))
   }
+  res <- res %>%
+      kableExtra::footnote(general_title = "", general = footnote)
   res
 }
---
+++ R/sds_image.R
@@ -0,0 +1,30 @@
+#' Render an image in either html or latex as appropriate.
+#'
+#' Insert an image as either html or latex
+#'
+#' @param img quoted relative path name of image
+#' @param width number giving width as percent
+#' @param height number giving height as percent
+#' @param lwidth number for latex width, if you want a different width for latex and for html
+#' @param lheight number for latex height, like `lwidth`
+#' @param format either `"html"` or `"latex"`. Default uses `knitr::is_latex_output()`
+#'
+#'
+#' @examples
+#' sds_image("images/bogus", format = "latex", width = 50)
+#'
+#' @export
+
+sds_image <- function(img, width=NULL, height=NULL, lwidth = width, lheight = height,
+                      format = ifelse(knitr::is_latex_output(), "latex", "html")){
+
+  if (format == "html") {
+    wstring <- ifelse(is.null(width), "", sprintf("width='%s%%'", as.character(width)))
+    hstring <- ifelse(is.null(height), "", sprintf("height='%s%%'", as.character(height)))
+    sprintf("<img src=%s %s %s>", img, wstring, hstring)
+  } else {
+    wstring <- ifelse(is.null(lwidth), "0.5", as.character(lwidth/100))
+    sprintf("\\includegraphics[width=%s\\textwidth]{%s}", wstring, img)
+  }
+
+}

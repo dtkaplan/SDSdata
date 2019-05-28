a3109b2f5777f70d9c7ccf7af30e5fb2494211eb
Danny Kaplan <kaplan@macalester.edu>
2019-05-28 14:31:20 -0500

    Added facility to make Word/PDF links automatically

--- R/blanks.R
+++ R/blanks.R
@@ -7,9 +7,19 @@
 #' Default: nonbreaking space followed by a period.
 #'
 #' @export
-question_blank <- function(prompts = "", length = 25, underline = "  .",
+question_blank <- function(prompts = "", length = 3, underline = "  .",
                            breaks = 2) {
   blank <- paste0(rep(underline, length), collapse = "")
   spaces <- paste0(rep("      \n\n", breaks), collapse = "")
   paste(c(paste(prompts, "\t",  blank), ""), collapse  = spaces)
+}
+
+#' @export
+word_pdf_links <- function() {
+  stub_name <- tools::file_path_sans_ext(knitr::current_input())
+  if (knitr::opts_knit$get('rmarkdown.pandoc.to') %in% c("html")) {
+    glue::glue("Get formatted versions: [Word](/word-versions/{stub_name}.docx) : [PDF](/pdf-versions/{stub_name}.pdf)")
+  } else {
+    " "
+  }
 }
